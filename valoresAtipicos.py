7913dbc9ccccf86228808652aa34de509dd2790a
sivaj <32196925+sivajota@users.noreply.github.com>
2025-07-11 19:43:43 +0200

    Update valoresAtipicos.py

--- valoresAtipicos.py
+++ valoresAtipicos.py
@@ -1,13 +1,20 @@
 #Escribir una función que reciba una muestra de números y devuelva los valores atípicos, es decir, los valores cuya puntuación típica sea mayor que 3 o menor que -3. 
 #Nota: La puntuación típica de un valor se obtiene restando la media y dividiendo por la desviación típica de la muestra.
 
-#Pseudo código, que se apoya en un modulo Math y un metodo deviation que retorna la desviación tipica de un elemento dada una lista de elementos, en este caso la muestra
-
 import MATH
 
 def atipicos(muestra):
 
-	for i in range(muestra.length):
+	suma = 0
+
+	for i in range(len(muestra)):
+		suma += muestra[i]
+
+	media = suma/len(muestra)
+
+	for i in range(len(muestra)):
 		valorTipico = (muestra[i] - media)/ MATH.Deviation(muestra)
+		#podria usar tambien algo asi y en ese caso la funcion anterior no haria falta
+		#valor valorTipico = (muestra[i] - MATH.Media(muestra))/ MATH.Deviation(muestra)
 		if valorTipico > 3 or puntuacion < -3:
 			print(valorTipico)
